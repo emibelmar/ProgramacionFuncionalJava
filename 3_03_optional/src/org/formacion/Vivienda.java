93bceb910bdbd240191de0cd5ed0725fa6e1c67a
emiliobeltran94@gmail.com <emiliobeltran94@gmail.com>
2021-03-29 15:20:17 +0200

    JDK functional interfaces & Optionals

---
+++ 3_03_optional/src/org/formacion/Vivienda.java
@@ -0,0 +1,33 @@
+package org.formacion;
+
+import java.util.Optional;
+
+public class Vivienda {
+
+	private Optional<Persona> propietario;
+
+	private String nombre;
+
+	public Vivienda(Persona propietario, String nombre) {
+		this.nombre = nombre;
+		this.propietario = Optional.ofNullable(propietario);
+	}
+
+	public Vivienda(String nombre) {
+		this.nombre = nombre;
+		this.propietario = Optional.empty();
+	}
+
+	public Optional<Persona> getPropietario() {
+		return propietario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Optional<String> nombrePropietario() {
+		return propietario.map(p -> p.getNombre());
+	}
+
+}
